98f8462f7cb51110e4c668597b1d597bfe9723b9
MartinDao23 <161600728+MartinDao23@users.noreply.github.com>
2024-04-26 21:10:18 +0000

    22-27(not 23)

--- exercises/exercise_27.py
+++ exercises/exercise_27.py
@@ -1,1 +1,15 @@
 # Your solution to Exercise 27
+n = input("Enter a string")
+former = n[0]
+count = 0
+output = n[0]
+for char in n:
+    if char == former:
+        count += 1
+    else:
+        output += str(count) 
+        output += char
+        count = 1
+        former = char
+output += str(count) 
+print(output)
